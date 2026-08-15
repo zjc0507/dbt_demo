6eb93e54f177061ecfd54fef26edab24946e8280
Jichi Zhang <zjc0507@gmail.com>
2026-08-15 08:36:20 +0000

    daliy-weather create

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,12 +1,30 @@
 WITH daily_weather as (
 
     select
-    *
+    date(time) as daily_weather,
+    weather,
+    temp,
+    pressure,
+    humidity,
+    clouds
     from {{source('demo','t_weather')}}
+    ),
 
- 
-    
+    daily_weather_agg as (
+    select 
+    daily_weather,
+    weather,
+    round(avg(temp),2) AS avg_temp,
+    round(avg(pressure),2) AS avg_pressure,
+    round(avg(humidity),2) AS avg_humidity,
+    round(avg(clouds),2) AS avg_clouds
+
+    from daily_weather
+
+    group by daily_weather,weather
+
+    qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY     count(weather) desc)  =1
 )
 
 select *
-from daily_weather   
+from daily_weather_agg
